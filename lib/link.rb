438959b612033d204ae6a1d935c0d45358694cc4
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-05 21:07:38 +0000

    added notes to link model

--- lib/link.rb
+++ lib/link.rb
@@ -1,8 +1,12 @@
+# This class corresponds to a table in the database
+# it is used to manipulate the data
+
 class Link
 
+  # this make sthe instances of this class Datamapper resources
   include DataMapper::Resource
 
-  property :id, Serial
+  property :id, Serial # Serial means that it will be auto-incremented for every record
   property :title, String
   property :url, String
 
