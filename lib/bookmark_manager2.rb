8258be5c144e6bd2edcf84c998b8389add5b6f66
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-07 12:54:22 +0000

    cucumber initialised

---
+++ lib/bookmark_manager2.rb
@@ -0,0 +1,10 @@
+require 'sinatra/base'
+
+class bookmark_manager2 < Sinatra::Base
+  get '/' do
+    'Hello bookmark_manager2!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
