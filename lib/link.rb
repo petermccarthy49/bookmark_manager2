f60fae025060fc5b04d198507e5a0344a8a1f2c1
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-07 12:33:06 +0000

    link comments aded

--- lib/link.rb
+++ lib/link.rb
@@ -1,7 +1,12 @@
+# this class corresponds to a table in the db
+# it's used to manipulate the data
+
 class Link
 
+  # this makes the instances of this class Datamapper resources
   include DataMapper::Resource
 
+  # this block describes what resiurces the model will have
   property :id      Serial
   property :title   String
   property :url     String
