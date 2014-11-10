aa9f64817f10727eb0f62b7c3c8508afbea427a9
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-10 15:41:00 +0000

    capybara configured

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe 'Link' do
 
   it 'can create a link and add it to the database' do
-    # expect(Link.count).to eq 0
+    expect(Link.count).to eq 0
     Link.create(:title => "google", :url => "http:///www.google.com/")
     expect(Link.count).to eq 1
   end
