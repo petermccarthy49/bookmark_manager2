ada9fd0fc7cfb6eced9f8e971f477f9615a86578
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-10 20:40:45 +0000

    silly typos

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -5,7 +5,7 @@ feature "User adds a new link" do
     expect(Link.count).to eq 0
     visit '/'
     add_link("http://www.google.com", "google")
-    expect(link.count).to eq 1
+    expect(Link.count).to eq 1
     link = Link.first
     expect(link.url).to eq "http://www.google.com"
     expect(link.title).to eq "google"
