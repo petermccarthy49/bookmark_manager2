fc608f86935e1b43c6297c7a2ccf571a1b639460
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-11 11:30:06 +0000

    tests for filtering added

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -4,12 +4,26 @@ feature "User browses the list of links" do
 
   before(:each) {
     Link.create(:url => "http://www.google.com", 
-                :title => "google")
+                :title => "google",
+                :tags => [Tag.first_or_create(:text => "search")])
+  
+    Link.create(:url => "http://www.bbc.co.uk", 
+                :title => "bbc",
+                :tags => [Tag.first_or_create(:text => "news")])
   }
+
 
   scenario 'when opening the homepage' do
     visit '/'
     expect(page).to have_content("google")
   end
+
+
+  scenario 'filtered by tag' do
+    visit '/tags/search'
+    expect(page).not_to have_content("bbc")
+    expect(page).to have_content("google")
+  end
+
 
 end
