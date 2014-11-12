34950b288c054c255507f184bf25b341e185bd9c
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-12 10:20:50 +0000

    error message tweaked

--- app/server.rb
+++ app/server.rb
@@ -48,7 +48,7 @@ post '/users' do
     session[:user_id] = @user.id
     redirect to('/')
   else
-    flash[:notice] = ["Sorry, your passwords don't match"]
+    flash[:notice] = "Sorry, your passwords don't match"
     erb :"users/new"
   end
 end
