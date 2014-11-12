21b92c1af83ef252990f6ab4fa88d621ba96a965
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-12 12:26:35 +0000

    sign_in method and second test added

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -51,5 +51,19 @@ feature "User signs in" do
     expect(page).to have_content("Welcome, b@b.com")
   end
 
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, b@b.com")
+    sign_in("b@b.com", "wrong")
+    expect(page).to have_content("Welcome, b@b.com")
+  end
+
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in :email, :with => email
+    fill_in :password, :with => password
+    click_button 'Sign in'
+  end
+
 
 end
