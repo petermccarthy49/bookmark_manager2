f15a823a978cc02b4cdefcc286729ceb9dd115fa
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-13 13:19:27 +0000

    mailgun added

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,3 +1,10 @@
-def send_email(user, token)
 
+def send_email(user, token)
+  RestClient.post "https://api:key-59420320c9fd6451dcef1331691e6a4a"\
+  "@api.mailgun.net/v2/sandbox6c2f98958b174e15aa6ff6688f7e0cb0.mailgun.org/messages",
+  :from => "Mailgun Sandbox <postmaster@sandbox6c2f98958b174e15aa6ff6688f7e0cb0.mailgun.org>",
+  :to => user.email,
+  :subject => "Bookmark Manager2 password reset",
+  :text => "Click on this link to reset your password. http://localhost:9292/users/reset_password/:#{token}"
 end
+
